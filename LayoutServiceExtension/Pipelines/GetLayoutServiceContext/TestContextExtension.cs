ee42420f8a714106ae5f5e60fe2d7d31d5c047b7
ivanbuzyka <ivanbuzyka@gmail.com>
2022-06-13 14:24:04 +0200

    initial

---
+++ LayoutServiceExtension/Pipelines/GetLayoutServiceContext/TestContextExtension.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.LayoutService.ItemRendering.Pipelines.GetLayoutServiceContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LayoutServiceExtension.Pipelines.GetLayoutServiceContext
+{
+  public class TestContextExtension : Sitecore.LayoutService.ItemRendering.Pipelines.GetLayoutServiceContext.IGetLayoutServiceContextProcessor
+  {
+    public void Process(GetLayoutServiceContextArgs args)
+    {
+      if (args.ContextData.ContainsKey("thirdpartyDataOne")) {
+        return;
+      }
+
+      args.ContextData.Add("thirdpartyDataOne", new
+      {
+        property1 = "This data should come from third party source",
+        property2 = "Another third-party data"
+      });
+    }
+  }
+}
---
+++ LayoutServiceExtension/RenderingContentsResolver/TestRenderingContentsResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.LayoutService.Configuration;
+using Sitecore.LayoutService.ItemRendering.ContentsResolvers;
+using Sitecore.Mvc.Presentation;
+
+namespace LayoutServiceExtension.RenderingContentsResolver
+{  
+  public class TestRenderingContentsResolver : Sitecore.LayoutService.ItemRendering.ContentsResolvers.RenderingContentsResolver
+  {
+    public override object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+    {
+      // here to get custom data from third-party data source
+      // below is the anonymous object to construct with the custom data
+      var customData = new
+      {
+        property1 = "property value 1",
+        property2 = "property value 2",
+        property3 = DateTime.Now
+      };
+
+      object json = base.ResolveContents(rendering, renderingConfig);
+
+      // wrapping both: ootb and custom data into one anonymous object
+      return new { fromContentResolver = json, customData = customData };
+    }
+
+  }
+}
